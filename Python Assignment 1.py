1bd6830a98d9516bed5c18da85eac5082a760188
karanchoudhary360 <74554565+karanchoudhary360@users.noreply.github.com>
2022-07-03 15:54:45 +0530

    Add files via upload

---
+++ Python Assignment 1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(2000,3201):
+    if i%7==0 and i%5!=0:
+        print(i,end=",")
+
+
+# In[9]:
+
+
+a=str(input("first name"))
+b=str(input("last name"))
+print(a[::-1]+" "+b[::-1])
+
+
+# In[11]:
+
+
+d=12
+r=d/2
+pie=22/7
+volume=(4/3)*pie*(r**3)
+print(volume)
+
+
+# In[ ]:
+
+
+
+
